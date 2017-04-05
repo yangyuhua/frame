11090570f9751446b0903ee56b9fc05083faefb4
Longder <longder1989@qq.com>
2017-04-05 15:08:25 +0800

    frame

---
+++ src/main/java/net/yyh/frame/model/Sample.java
@@ -0,0 +1,30 @@
+package net.yyh.frame.model;
+
+/**
+ *
+ */
+public class Sample {
+
+	private static final long serialVersionUID = -4075606447345640202L;
+
+	private String name;
+
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
