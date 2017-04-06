931f357bc6e2f67492ac28bf4fc51d9d06908c5c
Longder <longder1989@qq.com>
2017-04-06 16:59:58 +0800

    frame

--- src/test/java/net/yyh/test/BaseTest.java
+++ src/test/java/net/yyh/test/BaseTest.java
@@ -8,6 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by Administrator on 2017/4/5.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:/spring-mvc.xml","classpath:/root-context.xml"})
+@ContextConfiguration({"file:src/main/resources/spring-mvc.xml","file:src/main/resources/root-context.xml"})
 public class BaseTest {
 }
